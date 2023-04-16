389f006d24111b2ba9dd1eb0094689b6d0974d23
Medianto Pratama Pentadakosta <out1778@fifgroup.co.id>
2023-04-16 18:04:19 +0700

    set up

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        appDir: true,
+    },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        HOST: process.env.HOST,
+    }
+}
+
+module.exports = nextConfig
